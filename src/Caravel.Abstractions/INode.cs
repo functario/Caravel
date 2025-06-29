9fc1de91ee04fb0e3750c03054937113f8d6651c
Functario <166994080+functario@users.noreply.github.com>
2025-06-29 12:14:38 -0400

    Comment

--- src/Caravel.Abstractions/INode.cs
+++ src/Caravel.Abstractions/INode.cs
@@ -4,5 +4,12 @@ namespace Caravel.Abstractions;
 public interface INode
 {
     public ImmutableHashSet<IEdge> GetEdges();
+
+    /// <summary>
+    /// Method called each time a <see cref="INode"/> is opened.
+    /// </summary>
+    /// <param name="journey">The current <see cref="IJourney"/>.</param>
+    /// <param name="cancellationToken">The CancellationToken.</param>
+    /// <returns>A Task.</returns>
     public Task OnNodeOpenedAsync(IJourney journey, CancellationToken cancellationToken);
 }
