aedb252aa88c8e77e0731931cb1fc15ea07827a8
Functario <166994080+functario@users.noreply.github.com>
2025-07-04 11:21:27 -0400

    Add unweighted ExcludedNodes

---
+++ test/UnitTests/Caravel.Core.UnitTests/Tests/Navigation/Atomic/AtomicIgnoresExcludedNodes.cs
@@ -0,0 +1,55 @@
+﻿using Caravel.Tests.Fixtures.FixedJourneys;
+
+namespace Caravel.Core.UnitTests.Tests.Navigation.Atomic;
+
+[Trait(TestType, Unit)]
+[Trait(Feature, FeatureNavigation)]
+[Trait(Domain, ExcludedNodesDomain)]
+public class AtomicIgnoresExcludedNodes
+{
+    [Theory(DisplayName = "Whith single excluded node")]
+    [InlineData(typeof(Node2))]
+    [InlineData(typeof(Node3))]
+    public async Task Test1(Type excludedNode)
+    {
+        // Arrange
+        var builder = new JourneyBuilder()
+            .AddNode<Node1>()
+            .WithEdge<Node2>()
+            .WithEdge<Node3>()
+            .Done()
+            .AddNode<Node2>()
+            .WithEdge<Node4>()
+            .Done()
+            .AddNode<Node3>()
+            .WithEdge<Node4>()
+            .Done()
+            .AddNode<Node4>()
+            .Done();
+
+        var journey = builder.Build();
+        ExcludedNodes excludedNodes = [excludedNode];
+
+        // Act
+        var sut = await journey.GotoAsync<Node4>(excludedNodes);
+
+        // Assert
+        var result = await sut.ToMermaidSequenceDiagramMarkdownAsync(WithQuadrant);
+        await result.VerifyMermaidMarkdownAsync().UseParameters(excludedNode.Name);
+    }
+
+    [Fact(DisplayName = "Whith many excluded nodes")]
+    public async Task Test2()
+    {
+        // Arrange
+        var journey = JourneyFixtures.JourneyWithJoinRightFractalGraph3Levels.Build();
+        ExcludedNodes excludedNodes = [typeof(Node3), typeof(Node7)];
+
+        // Act
+        var sut = await journey.GotoAsync<Node14>(excludedNodes);
+
+        // Assert
+        var result = await sut.ToMermaidSequenceDiagramMarkdownAsync(WithQuadrant);
+        await result.VerifyMermaidMarkdownAsync();
+    }
+}
