b69565bd67cf795170e749f37d2fabd9fb762c48
Functario <166994080+functario@users.noreply.github.com>
2025-07-06 09:49:24 -0400

    WIP: add playwright test

---
+++ examples/PlaywrightDemo/WebSite.PlaywrightTests/PlawrightFixture.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Playwright;
+
+namespace WebSite.PlaywrightTests;
+
+[CollectionDefinition(nameof(PlaywrightCollection))]
+public class PlaywrightCollection : ICollectionFixture<PlaywrightFixture>
+{
+    // This class has no code, and is never created. Its purpose is simply
+    // to be the place to apply [CollectionDefinition] and all the
+    // ICollectionFixture<> interfaces.
+}
+
+public class PlaywrightFixture : IAsyncLifetime
+{
+    public IPlaywright Playwright { get; private set; } = null!;
+    public IBrowser Browser { get; private set; } = null!;
+    public IPage Page { get; private set; } = null!;
+
+    public async Task InitializeAsync()
+    {
+        Playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+
+        Browser = await Playwright.Chromium.LaunchAsync(BrowserTypeLaunchOptions);
+
+        Page = await Browser.NewPageAsync();
+    }
+
+    private static BrowserTypeLaunchOptions BrowserTypeLaunchOptions =>
+        new() { Headless = false, SlowMo = 500 };
+
+    public async Task DisposeAsync()
+    {
+        if (Browser != null)
+        {
+            await Browser.CloseAsync();
+        }
+
+        Playwright?.Dispose();
+    }
+}
---
+++ examples/PlaywrightDemo/WebSite.PlaywrightTests/Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Playwright;
+
+namespace WebSite.PlaywrightTests.Tests;
+
+[Collection(nameof(PlaywrightCollection))]
+public class UnitTest1
+{
+    private readonly PlaywrightFixture _playwrightFixture;
+    private readonly IPage _page;
+
+    public UnitTest1(PlaywrightFixture playwrightFixture)
+    {
+        _playwrightFixture = playwrightFixture;
+        _page = _playwrightFixture.Page;
+    }
+
+    [Fact]
+    public async Task Test1()
+    {
+        var filePath = Path.GetFullPath("WebSite.html");
+        var fileUrl = new Uri(filePath).AbsoluteUri;
+        await _page.GotoAsync(fileUrl);
+    }
+}
