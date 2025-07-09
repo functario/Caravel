8bc5ae4aa55e3e82149d64256830d95bb0fcae23
Functario <166994080+functario@users.noreply.github.com>
2025-07-09 06:46:47 -0400

    Add MCP tool

---
+++ examples/MCP/Caravel.MCP.Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Playwright;
+using WebSite.Facade;
+
+var builder = Host.CreateDefaultBuilder(args);
+
+var playwright = await Playwright.CreateAsync();
+var browser = await playwright.Chromium.LaunchAsync(new() { Headless = false, SlowMo = 1000 });
+var context = await browser.NewContextAsync();
+var page = await context.NewPageAsync();
+
+builder.ConfigureServices(
+    (context, services) =>
+    {
+        services.AddMcpServer().WithStdioServerTransport().WithToolsFromAssembly();
+        services
+            .AddOptions<AppOptions>()
+            .Bind(context.Configuration.GetSection(AppOptions.Name))
+            .ValidateOnStart();
+
+        services.ConfigureOptions<AppOptions>();
+        services.AddSingleton<IPage>(page);
+        services.AddWebSiteFacade(context);
+        services.AddScoped<WebSiteJourney>(x =>
+        {
+            var journeyBuilder = x.GetRequiredService<WebSiteJourneyBuilder>();
+            return journeyBuilder.Create(CancellationToken.None);
+        });
+    }
+);
+
+await builder.Build().RunAsync();
---
+++ examples/MCP/Caravel.MCP.Server/Tools/GotoTool.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+using ModelContextProtocol.Server;
+using WebSite.Facade;
+
+namespace Caravel.MCP.Server.Tools;
+
+[McpServerToolType]
+internal static class GotoTool
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"Hello from C#: {message}";
+
+    [
+        McpServerTool,
+        Description(
+            "Goto from the current opened page to another destination page"
+                + " using the name of type of the Page in WebSite."
+        )
+    ]
+    public static async Task<string> GotoAsync(WebSiteJourney journey, string destinationPage)
+    {
+        var webSiteAssembly = Assembly.GetAssembly(typeof(WebSiteJourney))!;
+        var type = webSiteAssembly.GetType(destinationPage)!;
+        Debug.WriteLine(type);
+
+        var method = typeof(WebSiteJourney)
+            .GetMethod("GotoAsync", BindingFlags.Instance | BindingFlags.Public)!
+            .MakeGenericMethod(type);
+
+        Debug.WriteLine(method);
+
+        // Invoke it and await the Task
+        var task = (Task)method.Invoke(journey, null)!;
+        await task;
+
+        // You can return something if needed; here we return the type name for illustration
+        return $"Navigated to {destinationPage}";
+    }
+}
