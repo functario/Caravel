ad50edca4f5bc366e1b57ddeae7cb54263bdb5a7
Functario <166994080+functario@users.noreply.github.com>
2025-08-06 08:04:26 -0400

    Remove unused name

--- examples/PlaywrightDemo/WebSite.Facade/Configurations/AppOptions.cs
+++ examples/PlaywrightDemo/WebSite.Facade/Configurations/AppOptions.cs
@@ -4,8 +4,6 @@ namespace WebSite.Facade.Configurations;
 
 public sealed class AppOptions : IValidateOptions<AppOptions>
 {
-    public const string Name = "AppOptions";
-
     public string WebSitePath { get; set; } = null!;
 
     public ValidateOptionsResult Validate(string? name, AppOptions options)
