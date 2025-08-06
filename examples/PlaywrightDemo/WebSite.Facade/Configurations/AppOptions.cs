55b21ca576423bae401cdce74e60124af0142199
Functario <166994080+functario@users.noreply.github.com>
2025-08-06 08:05:01 -0400

    Remove option validation

--- examples/PlaywrightDemo/WebSite.Facade/Configurations/AppOptions.cs
+++ examples/PlaywrightDemo/WebSite.Facade/Configurations/AppOptions.cs
@@ -1,18 +1,6 @@
-﻿using Microsoft.Extensions.Options;
+﻿namespace WebSite.Facade.Configurations;
 
-namespace WebSite.Facade.Configurations;
-
-public sealed class AppOptions : IValidateOptions<AppOptions>
+public sealed class AppOptions
 {
     public string WebSitePath { get; set; } = null!;
-
-    public ValidateOptionsResult Validate(string? name, AppOptions options)
-    {
-        if (string.IsNullOrWhiteSpace(options?.WebSitePath))
-        {
-            return ValidateOptionsResult.Fail("Missing path to 'WebSite.html'");
-        }
-
-        return ValidateOptionsResult.Success;
-    }
 }
