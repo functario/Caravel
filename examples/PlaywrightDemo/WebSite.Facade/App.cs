20de13d76fdb2bdd8eb9fc909618fa6f968319da
Functario <166994080+functario@users.noreply.github.com>
2025-08-07 07:46:24 -0400

    Display the weight of PageA->>|99|PageE. (#18)

--- examples/PlaywrightDemo/WebSite.Facade/App.cs
+++ examples/PlaywrightDemo/WebSite.Facade/App.cs
@@ -11,7 +11,7 @@ public sealed class App : IAsyncDisposable
         PageA->>PageB
         PageA->>PageC
         PageA->>PageD
-        PageA->>PageE
+        PageA->>|99|PageE
 
         PageB->>PageA
         PageB->>PageC
