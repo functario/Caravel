17f492d2536f5b3aeca3d0930db0e0c2b1bccbdc
Functario <166994080+functario@users.noreply.github.com>
2025-08-09 14:39:00 -0400

    add scripts playwright

---
+++ examples/PlaywrightDemo/WebSite.PlaywrightTests/scripts/install-playwright.ps1
@@ -0,0 +1,13 @@
+$targetPath = "\bin\Debug\net9.0"
+$scriptsPath = "\bin\Debug\net9.0\Scripts"
+$currentPath = Get-Location
+
+if ($currentPath.Path.EndsWith($scriptsPath)) {
+    $playwrightPath = "..\playwright.ps1"
+} elseif ($currentPath.Path.EndsWith($targetPath)) {
+    $playwrightPath = ".\playwright.ps1"
+} else {
+    $playwrightPath = Join-Path -Path $PSScriptRoot -ChildPath "..\$targetPath\playwright.ps1"
+}
+
+& $playwrightPath install --with-deps chromium
