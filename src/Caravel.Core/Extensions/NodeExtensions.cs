467109908397d873f3ad6f20cce3c6e3df0b8ea8
Functario <166994080+functario@users.noreply.github.com>
2025-08-16 11:07:11 -0400

    Remove enforing to write returned type from CreateEdge.

--- src/Caravel.Core/Extensions/NodeExtensions.cs
+++ src/Caravel.Core/Extensions/NodeExtensions.cs
@@ -7,19 +7,17 @@ public static class NodeExtensions
     /// <summary>
     /// Create an <see cref="IEdge"/>.
     /// </summary>
-    /// <typeparam name="TNeighbor">The type of the neighbor. It is mandatory and cannot be inferred.</typeparam>
+    /// <typeparam name="TNeighbor">The type of the neighbor</typeparam>
     /// <param name="origin">The origin <see cref="INode"/>.</param>
     /// <param name="moveNext">The function to move to the neighbor.</param>
     /// <param name="weight">The weight of the <see cref="IEdge"/>.</param>
     /// <param name="metaData">The metaData of the <see cref="IEdge.NeighborNavigator"/>.</param>
-    /// <param name="_">Unused parameter preventing inference to ensure match between <typeparamref name="TNeighbor"/> and <paramref name="moveNext"/>.</param>
     /// <returns>The <see cref="IEdge"/>.</returns>
     public static IEdge CreateEdge<TNeighbor>(
         this INode origin,
         Func<IJourney, CancellationToken, Task<TNeighbor>> moveNext,
         int weight = 0,
-        object? metaData = null,
-        TNeighbor _ = default!
+        object? metaData = null
     )
         where TNeighbor : INode
     {
