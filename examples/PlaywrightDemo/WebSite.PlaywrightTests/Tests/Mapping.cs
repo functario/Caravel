e497cba03620976b307b320e3816e0e7308b9d0a
Functario <166994080+functario@users.noreply.github.com>
2025-08-23 08:26:09 -0400

    Test being able to get Pages instances from Map

---
+++ examples/PlaywrightDemo/WebSite.PlaywrightTests/Tests/Mapping.cs
@@ -0,0 +1,25 @@
+﻿using AwesomeAssertions;
+using WebSite.Facade;
+using WebSite.Facade.POMs.Pages;
+
+namespace WebSite.PlaywrightTests.Tests;
+
+public sealed class Mapping : TestBase
+{
+    public Mapping(PlaywrightFixture playwrightFixture)
+        : base(playwrightFixture) { }
+
+    [Fact(DisplayName = "Get pages from Map")]
+    public async Task Test1()
+    {
+        await WebSiteJourney.App.OpenWebSiteAsync("", JourneyCTSource.Token);
+
+        var map = WebSiteJourney.Map;
+
+        map.PageA.Should().NotBeNull().And.BeOfType<PageA>();
+        map.PageB.Should().NotBeNull().And.BeOfType<PageB>();
+        map.PageC.Should().NotBeNull().And.BeOfType<PageC>();
+        map.PageD.Should().NotBeNull().And.BeOfType<PageD>();
+        map.PageE.Should().NotBeNull().And.BeOfType<PageE>();
+    }
+}
