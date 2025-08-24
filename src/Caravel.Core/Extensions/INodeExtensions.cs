080e207742aca4e8d7f7ed257ac0a6f54d3a8ac2
Functario <166994080+functario@users.noreply.github.com>
2025-08-24 09:18:32 -0400

    Document why extension method CreateEdge<TNeighbor> is not for custom implementation.

--- src/Caravel.Core/Extensions/INodeExtensions.cs
+++ src/Caravel.Core/Extensions/INodeExtensions.cs
@@ -5,14 +5,17 @@ namespace Caravel.Core.Extensions;
 public static class INodeExtensions
 {
     /// <summary>
-    /// Create an <see cref="IEdge"/>.
+    /// Create an <see cref="Edge"/>.
     /// </summary>
     /// <typeparam name="TNeighbor">The type of the neighbor</typeparam>
     /// <param name="origin">The origin <see cref="INode"/>.</param>
     /// <param name="moveNext">The function to move to the neighbor.</param>
     /// <param name="weight">The weight of the <see cref="IEdge"/>.</param>
-    /// <param name="metaData">The metaData of the <see cref="IEdge.NeighborNavigator"/>.</param>
+    /// <param name="metaData">The metaData to add to the <see cref="NeighborNavigator"/>.</param>
     /// <returns>The <see cref="IEdge"/>.</returns>
+    /// <remarks>This helper method used the default <see cref="Edge"/> and <see cref="NeighborNavigator"/>.
+    /// Do not use for custom <see cref="IEdge"/> or <see cref="INeighborNavigator"/>.
+    /// </remarks>
     public static IEdge CreateEdge<TNeighbor>(
         this INode origin,
         Func<IJourney, CancellationToken, Task<TNeighbor>> moveNext,
