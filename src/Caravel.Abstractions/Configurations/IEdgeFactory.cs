e18d84ada898bbc11d9a9127311b4c446a6c4378
Functario <166994080+functario@users.noreply.github.com>
2025-08-24 09:20:27 -0400

    Document why extension method CreateEdge<TNeighbor> is not for custom implementation. (#34)

--- src/Caravel.Abstractions/Configurations/IEdgeFactory.cs
+++ src/Caravel.Abstractions/Configurations/IEdgeFactory.cs
@@ -11,5 +11,12 @@ public interface IEdgeFactory
     /// returned from the <see cref="IEdge"/> must contains the same instance than <paramref name="selfNode"/>.</remarks>
     public IEdge CreteSelfEdge(INode selfNode);
 
+    /// <summary>
+    /// Create a <see cref="IEdge"/> with no weight.
+    /// </summary>
+    /// <param name="origin">The point of origin.</param>
+    /// <param name="destination">The point of destination.</param>
+    /// <param name="actionMetaData">The optional <see cref="IActionMetaData"/>.</param>
+    /// <returns>An <see cref="IEdge"/> returning an instance of the <paramref name="destination"/>.</returns>
     public IEdge CreateEdge(INode origin, INode destination, IActionMetaData? actionMetaData);
 }
